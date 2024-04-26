af2e797cd639a232c64c668637794a440bd58453
Jaden Pereira <jaden.pereira@gmail.com>
2024-04-25 23:29:01 -0400

    Initial commit of ReactJS files only

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from 'react';
+import Vex from 'vexflow';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const VF = Vex.Flow;
+
+const App = () => {
+  const divRef = useRef();
+
+  useEffect(() => {
+    if (divRef.current) {
+      // Fetch the JSON data from the public folder
+      fetch(process.env.PUBLIC_URL + '/notes.json')
+        .then(response => response.json())
+        .then(data => {
+
+          // Create a Factory, EasyScore, and System
+          const vf = new VF.Factory({ renderer: { elementId: divRef.current, width: 500, height: 1000 }, });
+          const score = vf.EasyScore();
+          const system = vf.System();
+
+          // Loop through the data
+          data.forEach((item, index) => {
+            // Add a stave with the voices to the system for each line of music
+            system.addStave({
+              voices: [
+                score.voice(score.notes(item.notes, {clef: item.clef, stem: item.stem})),
+              ],
+            }).addClef(item.clef).addTimeSignature('4/4');
+           });
+
+          // Draw the score
+          vf.draw();
+        });
+    }
+  }, []);
+
+  return <div ref={divRef} />;
+};
 
 export default App;
